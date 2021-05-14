f79bc2a00deb656f5b7c75e147df89728b01cb61
ayumuSasagawa <a_sasagawa@uluru.jp>
2021-05-14 22:04:07 +0900

    アソシエーションやってみた

---
+++ database/migrations/20210514120905-create-team.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Teams', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      ownerId: {
+        type: Sequelize.INTEGER,
+        reference: { model: 'Users', key: 'id' } 
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Teams');
+  }
+};
