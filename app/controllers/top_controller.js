f589e4b9d5b4911d51b33ee5ce3d15e8dd7225f9
ayumuSasagawa <a_sasagawa@uluru.jp>
2021-05-24 13:05:50 +0900

    いらない部分削除

--- app/controllers/top_controller.js
+++ app/controllers/top_controller.js
@@ -7,8 +7,7 @@ const User = models.User;
 class TopController extends Controller {
   async index(req, res) {
     if(req.user) {
-      const userId = req.user.id;
-      const user = await User.findByPk(userId);
+      const user = await User.findByPk(req.user.id);
       const tasks = await user.getAssignedTask({
         order: [['id', 'ASC']]
       });
@@ -20,18 +19,5 @@ class TopController extends Controller {
       res.render('index', { title: 'Express', user: req.user });
     }
   }
-
-  //ここ以降使わないから後で削除
-  async create(req, res) {
-    const teamId = req.params.team;
-    const team = await Team.findByPk(teamId);
-    const members = await team.getTeamMember({
-      include: { model: User, as: "User" },
-      order: [['id', 'ASC']]
-    });
-    res.render('manager/tasks/create', { teamId: teamId, members: members } );
-  }
-
 }
-
 module.exports = TopController;
