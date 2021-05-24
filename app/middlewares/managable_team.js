05ae786e65e062efad3b34690274adadd12f3db4
ayumuSasagawa <a_sasagawa@uluru.jp>
2021-05-24 20:33:05 +0900

    roleの型を変更して1と比較できるようにした（true or falseを返すところ）

--- app/middlewares/managable_team.js
+++ app/middlewares/managable_team.js
@@ -6,7 +6,6 @@ module.exports = async function managableTeam(req, res, next) {
 
   const user = await User.findByPk(req.user.id);
   const team = await Team.findByPk(req.params.team);
-
   if (!await team.isManager(user)) {
     await req.flash('alert', 'アクセスできません');
     res.redirect('/');
