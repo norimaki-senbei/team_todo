4cac1abe673ccaa81d30cdbf5963974827d781be
ayumuSasagawa <a_sasagawa@uluru.jp>
2021-05-25 12:59:34 +0900

    テーブルの作成が完了

---
+++ database/migrations/20210525035405-create-comment.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      taskId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Tasks', key: 'id' }
+      },
+      creatorId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Users', key: 'id' }
+      },
+      message: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      kind: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Comments');
+  }
+};
